0599fac4167dc8fee094247f0ead9b4311cab0f2
AdamW <barcelona1999.aw@gmail.com>
2021-04-10 15:33:36 +0200

    Table cases look

--- covid_prediction_app/lib/showtablefragment.dart
+++ covid_prediction_app/lib/showtablefragment.dart
@@ -18,8 +18,7 @@ class _ShowTableFragmentState extends State<ShowTableFragment> {
 
 
   Widget titleSection = Container(
-    child:Column(
-
+    child: Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget>[
         Padding(
@@ -31,25 +30,157 @@ class _ShowTableFragmentState extends State<ShowTableFragment> {
                 color: new Color(0xFFB71C1C),
                 fontWeight:FontWeight.bold,
                 fontSize: 30.0,
-
-                //backgroundColor: new Color(0xFFFF7043)
               ),
             )
         ),
 
         Padding(
-            padding:const EdgeInsets.all(160.0),
-            child:Text(
-                "Place for table" //$val// countryforchart.text,
+            padding:const EdgeInsets.all(0.01),
+
+           child: SingleChildScrollView(
+
+             scrollDirection: Axis.horizontal,
+
+             child: DataTable(
+               headingRowColor:
+               MaterialStateColor.resolveWith((states) => Colors.blue),
+                  columnSpacing: 30.0,
+                  showBottomBorder: true,
+
+               columns: [
+
+                  DataColumn(label: Text("Country:",style: TextStyle(fontWeight:FontWeight.bold,fontSize:17.0,fontStyle: FontStyle.italic,color:Colors.white),),tooltip: "Countries with covid data",),
+                  DataColumn(label: Text("Predicted cases:",style: TextStyle(fontWeight:FontWeight.bold,fontSize:17.0,fontStyle: FontStyle.italic,color:Colors.white),),tooltip: "Prediction of covid amount infection"),
+                  DataColumn(label: Text("Real cases:",style: TextStyle(fontWeight:FontWeight.bold,fontSize:17.0,fontStyle: FontStyle.italic,color:Colors.white),),tooltip: "Reality of covid amount infection"),
+               ],
+
+               rows: [
+
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                     cells: [
+                       DataCell(Text("Afghanistan",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                       DataCell(Text("30190",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                       DataCell(Text("28423",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                 ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Albania",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("1862",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("1890",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
+                 DataRow(
+                   color: MaterialStateProperty.resolveWith<Color>(
+                           (Set<MaterialState> states) {
+                         return Colors.white;
+                       }),
+                   cells: [
+                     DataCell(Text("Alberta|Canada",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.black))),
+                     DataCell(Text("8449",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.green))),
+                     DataCell(Text("7672",style:TextStyle(fontWeight: FontWeight.bold,fontSize:16.0,color: Colors.red))),
+                   ],
+                 ),
 
 
-            )
+               ],
+             ),
+
+               ),
+
         ),
 
 
 
 
       ],
+
 
     ),
 
